861535713751fc768a3a4f38b73e6583e4f5bd8a
Maykon Sousa <maykon.sousa@hotmail.com>
2023-08-07 08:31:39 -0300

    feat: configure deploy vercel

---
+++ config/plugins.ts
@@ -0,0 +1,12 @@
+export default ({ env }) => ({
+    "vercel-deploy": {
+      enabled: true,
+      config: {
+        deployHook: env('VERCEL_DEPLOY_PLUGIN_HOOK'),
+        apiToken: env('VERCEL_DEPLOY_PLUGIN_API_TOKEN'),
+        appFilter: env('VERCEL_DEPLOY_PLUGIN_APP_FILTER'),
+        teamFilter: env('VERCEL_DEPLOY_PLUGIN_TEAM_FILTER'),
+        roles: ["strapi-super-admin"],
+      },
+    },
+  });
